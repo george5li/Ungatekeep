68cc5e10d28507dd76a1b41490ad818a9a843230
Ryan Reddy <ryangreddy@gmail.com>
2025-09-21 15:20:40 +0000

    Revert to before I asked for serifs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Times New Roman', 'Times', 'serif'],
-        headline: ['Times New Roman', 'Times', 'serif'],
+        body: ['Helvetica', 'Arial', 'sans-serif'],
+        headline: ['Helvetica', 'Arial', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
